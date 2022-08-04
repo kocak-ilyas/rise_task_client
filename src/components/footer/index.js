1226464b382fc851591a332a495088230dc1549f
kocak-ilyas <kocakilyas18@gmail.com>
2022-08-04 22:22:20 +0300

    footer

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -1,11 +1,29 @@
 import React from "react";
+import logo from "../../assets/logo.png";
+
 import "./style.scss";
 
 const Footer = () => {
   return (
-    <div>
-      <hr />
-      <h1>FOOTER</h1>
+    <div className='container' style={{ marginTop: `150px` }}>
+      <footer className='py-3 my-4'>
+        <ul className='nav justify-content-center border-bottom pb-3 mb-3'>
+          <li className='nav-item'>
+            <a href='/' className='nav-link px-2 text-muted'>
+              Home
+            </a>
+          </li>
+          <li className='nav-item'>
+            <a href='/contact' className='nav-link px-2 text-muted'>
+              Contact
+            </a>
+          </li>
+        </ul>
+        <p className='text-center text-muted'>
+          <img src={logo} alt='logo' style={{ width: `18px`, marginRight: "20px", marginTop: "-9px" }} /> &copy; 2022
+          Rise Company, Inc
+        </p>
+      </footer>
     </div>
   );
 };
